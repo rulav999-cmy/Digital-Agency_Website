3578c9093b3dd53fd6f1b906fd1c8f7f8f24a95d
rulav999-cmy <rulav999@gmail.com>
2026-09-06 14:45:52 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelector(".menu-button");
+const navLinks = document.querySelector(".nav-links");
+
+menuButton.addEventListener("click", function () {
+
+    navLinks.classList.toggle("active");
+
+});
